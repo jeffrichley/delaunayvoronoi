69426562626e38a47e0ef4973daad2f8de59a5ec
jeffrichley <jeffrichley@gmail.com>
2016-01-20 15:57:29 -0500

    adding generics

--- src/com/infinity/delaunayvoronoi/model/PanGraph.java
+++ src/com/infinity/delaunayvoronoi/model/PanGraph.java
@@ -9,76 +9,36 @@ import java.util.List;
  * 
  * @author jeffreyrichley
  */
-public class PanGraph {
+public class PanGraph<R extends Polygon, S extends Arc, T extends Node> {
 
 	/**
 	 * All <code>Polygon</code>s in the graph
 	 */
-	private List<Polygon> polygons = new ArrayList<Polygon>();
+	private List<R> polygons = new ArrayList<R>();
 	
 	/**
 	 * All <code>Arc</code>s in the graph
 	 */
-	private List<Arc> arcs = new ArrayList<Arc>();
+	private List<S> arcs = new ArrayList<S>();
 	
 	/**
 	 * All <code>Node</code>s in the graph
 	 */
-	private List<Node> nodes = new ArrayList<Node>();
+	private List<T> nodes = new ArrayList<T>();
 
 	/**
 	 * Get the list of all <code>Polygon</code>s
 	 * @return An unmodifiable <code>List</code> of all the <code>Polygon</code>s
 	 */
-	public List<Polygon> getPolygons() {
+	public List<R> getPolygons() {
 		return Collections.unmodifiableList(polygons);
-	}
-
-	/**
-	 * Get the list of all <code>Arc</code>s
-	 * @return An unmodifiable <code>List</code> of all the <code>Arc</code>s
-	 */
-	public List<Arc> getArcs() {
-		return Collections.unmodifiableList(arcs);
-	}
-
-	/**
-	 * Get the list of all <code>Node</code>s
-	 * @return An unmodifiable <code>List</code> of all the <code>Node</code>s
-	 */
-	public List<Node> getNodes() {
-		return Collections.unmodifiableList(nodes);
-	}
-
-	/**
-	 * Add a <code>Node</code> to the graph
-	 * @param node The <code>Node</code> to add
-	 */
-	public void addNode(Node node) {
-		nodes.add(node);
-	}
-
-	/**
-	 * Add a <code>Arc</code> to the graph
-	 * @param arc The <code>Arc</code> to add
-	 */
-	public void addArc(Arc arc) {
-		arcs.add(arc);
-	}
-
-	/**
-	 * Removes the <code>Arc</code> from the graph
-	 * @param arc The <code>Arc</code> to remove
-	 */
-	public void removeArc(Arc arc) {
-		arcs.remove(arc);
 	}
 
 	/**
 	 * Add a <code>Polygon</code> to the graph
 	 * @param polygon The <code>Polygon</code> to add
 	 */
-	public void addPolygon(Polygon polygon) {
+	public void addPolygon(R polygon) {
 		polygons.add(polygon);
 	}
 
@@ -86,15 +46,55 @@ public class PanGraph {
 	 * Removes the <code>Polygon</code> from the graph 
 	 * @param polygon The <code>Polygon</code> to remove
 	 */
-	public void removePolygon(Polygon polygon) {
+	public void removePolygon(R polygon) {
 		polygons.remove(polygon);
+	}
+
+	/**
+	 * Get the list of all <code>Arc</code>s
+	 * @return An unmodifiable <code>List</code> of all the <code>Arc</code>s
+	 */
+	public List<S> getArcs() {
+		return Collections.unmodifiableList(arcs);
+	}
+
+	/**
+	 * Add a <code>Arc</code> to the graph
+	 * @param arc The <code>Arc</code> to add
+	 */
+	public void addArc(S arc) {
+		arcs.add(arc);
+	}
+
+	/**
+	 * Removes the <code>Arc</code> from the graph
+	 * @param arc The <code>Arc</code> to remove
+	 */
+	public void removeArc(S arc) {
+		arcs.remove(arc);
+	}
+
+	/**
+	 * Get the list of all <code>Node</code>s
+	 * @return An unmodifiable <code>List</code> of all the <code>Node</code>s
+	 */
+	public List<T> getNodes() {
+		return Collections.unmodifiableList(nodes);
+	}
+
+	/**
+	 * Add a <code>Node</code> to the graph
+	 * @param node The <code>Node</code> to add
+	 */
+	public void addNode(T node) {
+		nodes.add(node);
 	}
 
 	/**
 	 * Removes the <code>Node</code> from the graph 
 	 * @param node The <code>Node</code> to remove
 	 */
-	public void removeNode(Node node) {
+	public void removeNode(T node) {
 		nodes.remove(node);
 	}
 	
