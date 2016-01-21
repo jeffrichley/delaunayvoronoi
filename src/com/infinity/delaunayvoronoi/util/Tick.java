ddde615d65e3b788fcad4a2713c4552d52611ec6
jeffrichley <jeffrichley@gmail.com>
2016-01-21 10:48:28 -0500

    Documented

--- src/com/infinity/delaunayvoronoi/util/Tick.java
+++ src/com/infinity/delaunayvoronoi/util/Tick.java
@@ -1,15 +1,26 @@
 package com.infinity.delaunayvoronoi.util;
 
+/**
+ * Used to time actions in the code for speed increases and benchmarking
+ * @author Jeffrey.Richley
+ */
 public class Tick {
 
 	private long start;
 	private long last;
 
+	/**
+	 * Creates a new <code>Tick</code>
+	 */
 	public Tick() {
 		this.start = System.currentTimeMillis();
 		this.last = start;
 	}
 	
+	/**
+	 * Print that we have accomplished something and how long it took
+	 * @param msg The message to pring
+	 */
 	public void tock(String msg) {
 		long end = System.currentTimeMillis();
 		long sinceStart = end - start;
